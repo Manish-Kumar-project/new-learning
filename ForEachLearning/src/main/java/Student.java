b22dc0ffbf5dfd408b6e08e3fc01bf725651a62f
MANISH KUMAR <manishkumarbodla22@gmail.com>
2022-06-28 10:44:26 +0530

    code changes

--- ForEachLearning/src/main/java/Student.java
+++ ForEachLearning/src/main/java/Student.java
@@ -19,6 +19,70 @@ public class Student {
         this.total = total;
     }
 
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Integer studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGmail() {
+        return gmail;
+    }
+
+    public void setGmail(String gmail) {
+        this.gmail = gmail;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getStdClass() {
+        return stdClass;
+    }
+
+    public void setStdClass(Integer stdClass) {
+        this.stdClass = stdClass;
+    }
+
+    public Integer getMarks() {
+        return marks;
+    }
+
+    public void setMarks(Integer marks) {
+        this.marks = marks;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
     @Override
     public String toString() {
         return "Student{" +
--- ForEachLearning/src/main/java/StudentService.java
+++ ForEachLearning/src/main/java/StudentService.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StudentService {
 
@@ -7,6 +8,11 @@ public class StudentService {
         StudentRepository studentRepository =new StudentRepository();
         List<Student> studentList = new ArrayList<>();
         studentList = studentRepository.getAllStudents();
+        studentList.forEach(student -> {
+            System.out.println(student.getStudentId()+" "+student.getFirstName()+" "+student.getLastName()+" "+student.getAge()+" "+student.getGmail()+" "+student.getMarks()+" "+student.getTotal());
+        });
+        List<Student> students = studentList.stream().filter(student -> student.getFirstName().equals("james"))
+                .collect(Collectors.toList());
         studentList.forEach(System.out::println);
     }
 }
