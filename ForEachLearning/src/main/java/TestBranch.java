8f108398071e09853c999219d28fe33c2d4b5bf2
MANISH KUMAR <manishkumarbodla22@gmail.com>
2023-06-06 10:53:39 +0530

    commit two

--- ForEachLearning/src/main/java/TestBranch.java
+++ ForEachLearning/src/main/java/TestBranch.java
@@ -1,5 +1,14 @@
 public class TestBranch {
+
+    public void add(int a, int b) {
+        int a1 = a;
+        int b1 = b;
+        System.out.println("addition of two numbers : "+a1+b1);
+
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello Test");
+        new TestBranch().add(10,10);
     }
 }
