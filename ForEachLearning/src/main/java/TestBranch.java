6202ba324238c5f9a21a4067b37c4c6a982bce31
MANISH KUMAR <manishkumarbodla22@gmail.com>
2023-06-06 11:44:12 +0530

    commit seven

--- ForEachLearning/src/main/java/TestBranch.java
+++ ForEachLearning/src/main/java/TestBranch.java
@@ -59,9 +59,14 @@ public class TestBranch {
 
     }
      public void testStashFromRepo2(int a, int b) {
+         int a1 = a;
+         int b1 = b;
+         System.out.println("addition of two numbers : " + (a1 / b1));
+     }
+    public void testStashFromLocal2(int a, int b) {
         int a1 = a;
         int b1 = b;
-        System.out.println("addition of two numbers : "+(a1/b1));
+        System.out.println("addition of two numbers : " + (a1 % b1));
 
     }
     public static void main(String[] args) {
@@ -71,6 +76,7 @@ public class TestBranch {
         new TestBranch().mutliply(10, 2);
         new TestBranch().sub(10, 10);
         new TestBranch().testStashFromRepo2(10, 10);
+        new TestBranch().testStashFromLocal2(10, 10);
 
     }
 }
