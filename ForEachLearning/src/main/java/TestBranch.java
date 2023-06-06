3e92abbdee28e3603ed009084541a13b22326f12
MANISH KUMAR <manishkumarbodla22@gmail.com>
2023-06-06 11:19:04 +0530

    commit five

--- ForEachLearning/src/main/java/TestBranch.java
+++ ForEachLearning/src/main/java/TestBranch.java
@@ -3,34 +3,43 @@ public class TestBranch {
     public void add(int a, int b) {
         int a1 = a;
         int b1 = b;
-        System.out.println("addition of two numbers : "+a1+b1);
+        System.out.println("addition of two numbers : " + a1 + b1);
 
     }
+
     public void mutliply(int a, int b) {
         int a1 = a;
         int b1 = b;
-        System.out.println("addition of two numbers : "+a1*b1);
+        System.out.println("addition of two numbers : " + a1 * b1);
 
     }
-   public void sub(int a, int b) {
+
+    public void sub(int a, int b) {
         int a1 = a;
         int b1 = b;
-        System.out.println("addition of two numbers : "+(a1-b1));
+        System.out.println("addition of two numbers : " + (a1 - b1));
 
     }
-     public void div(int a, int b) {
+
+    public void div(int a, int b) {
         int a1 = a;
         int b1 = b;
-        System.out.println("addition of two numbers : "+(a1/b1));
+        System.out.println("addition of two numbers : " + (a1 / b1));
+    }
+
+    public void mod(int a, int b) {
+        int a1 = a; 
+        int b1 = b;
+        System.out.println("addition of two numbers : " + (a1 % b1));
 
     }
 
     public static void main(String[] args) {
         System.out.println("Hello Test");
-        new TestBranch().add(10,10);
-        new TestBranch().mutliply(10,10);
-        new TestBranch().mutliply(10,2);
-        new TestBranch().sub(10,10);
+        new TestBranch().add(10, 10);
+        new TestBranch().mutliply(10, 10);
+        new TestBranch().mutliply(10, 2);
+        new TestBranch().sub(10, 10);
 
     }
 }
