ef6e800521429c77c99be599e782da3004bcf0f4
Manish-Kumar-project <99067558+Manish-Kumar-project@users.noreply.github.com>
2023-06-08 12:13:08 +0530

    Update TestBranch.java

--- ForEachLearning/src/main/java/TestBranch.java
+++ ForEachLearning/src/main/java/TestBranch.java
@@ -116,6 +116,11 @@ public class TestBranch {
          int b1 = b;
          System.out.println("addition of two numbers : " + (a1 / b1));
      }
+    public void testStashFromRepo8(int a, int b) {
+         int a1 = a;
+         int b1 = b;
+         System.out.println("addition of two numbers : " + (a1 / b1));
+     }
     public static void main(String[] args) {
         System.out.println("Hello Test");
         new TestBranch().add(10, 10);
